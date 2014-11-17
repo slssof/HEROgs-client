6e661817f3c5f99b92b49ff619257e616e0b84ae
Sergey V. Glushko <dracorobus@gmail.com>
2014-11-17 14:15:13 +0800

    все работает из под ижика

--- js/foo.js
+++ js/foo.js
@@ -8,9 +8,9 @@ function modal(id_mod) {
         var maskWidth = $(window).width();
 
         $('#mask').css({'width': maskWidth, 'height': maskHeight});
-        $('#bg').addClass('blur');
-        $('#mask').fadeIn(1000);
-        //$('#mask').fadeTo("slow", 0.5);
+        //$('#bg').addClass('blur');
+        $('#mask').fadeIn(500);
+        $('#mask').fadeTo("slow", 0.7);
 
         var winH = $(window).height();
         var winW = $(window).width();
@@ -18,18 +18,18 @@ function modal(id_mod) {
         $(id).css('top', winH / 2 - $(id).height() / 2);
         $(id).css('left', winW / 2 - $(id).width() / 2);
 
-        $(id).fadeIn(2000);
+        $(id).fadeIn(1000);
 
 
     $('.close').click(function (e) {
         //e.preventDefault();
-        $('#bg').removeClass('blur');
-        $('#mask, .window').fadeOut(1000);
+        //$('#bg').removeClass('blur');
+        $('#mask, .window').fadeOut(500);
     });
 
     $('#mask').click(function () {
-        $('#bg').removeClass('blur');
-        $(this).fadeOut(1000);
-        $('.window').fadeOut(1000);
+        //$('#bg').removeClass('blur');
+        $(this).fadeOut(500);
+        $('.window').fadeOut(500);
     });
 }
